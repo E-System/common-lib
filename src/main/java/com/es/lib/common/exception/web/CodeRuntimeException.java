9ba8ae2532828ea27863e2c0d20d8367010826dc
Savchenko Vitaliy <savchenko.v@ext-system.com>
2019-06-18 13:15:47 +0700

    getter and setter

--- src/main/java/com/es/lib/common/exception/web/CodeRuntimeException.java
+++ src/main/java/com/es/lib/common/exception/web/CodeRuntimeException.java
@@ -28,6 +28,10 @@ public class CodeRuntimeException extends RuntimeException {
         this.code = code;
     }
 
+    public String getCode() { return code; }
+
+    public void setCode(String code) { this.code = code; }
+
     @Override
     public String toString() {
         return "CodeRuntimeException{" +
