77621f034627ac058facce8a246c4fac1807eeaa
diman <zuzoev.d@ext-system.com>
2020-02-15 17:31:49 +0700

    Add network classes

---
+++ src/main/java/com/es/lib/common/network/Proxy.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 E-System LLC
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.es.lib.common.network;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author Dmitriy Zuzoev - zuzoev.d@ext-system.com
+ * @since 15.02.2020
+ */
+@Getter
+@ToString
+@RequiredArgsConstructor
+public class Proxy {
+
+    private final java.net.Proxy.Type type;
+    private final String host;
+    private final int port;
+
+    public Proxy(String host, int port) {
+        this(java.net.Proxy.Type.HTTP, host, port);
+    }
+
+    public java.net.Proxy toNative() {
+        return new java.net.Proxy(getType(), new InetSocketAddress(getHost(), getPort()));
+    }
+}
---
+++ src/test/groovy/com/es/lib/common/network/KeyStoreSpec.groovy
@@ -0,0 +1,36 @@
+package com.es.lib.common.network
+
+import spock.lang.Specification
+
+import java.nio.file.Paths
+
+class KeyStoreSpec extends Specification {
+
+    def "Key type by extension"() {
+        expect:
+        KeyStore.create(Paths.get("/tmp/test.jks"), '', '').type == 'JKS'
+        KeyStore.create(Paths.get("/tmp/test.p12"), '', '').type == 'PKCS12'
+        KeyStore.create(Paths.get("/tmp/test.pfx"), '', '').type == 'PKCS12'
+    }
+
+    def "Invalid key type by extension"() {
+        when:
+        KeyStore.create(Paths.get("/tmp/test.jks1"), '', '')
+        then:
+        def ex = thrown(IllegalArgumentException)
+        ex.message == 'Unable to determine key type by file: /tmp/test.jks1'
+    }
+
+    def "Invalid key type by empty extension"() {
+        when:
+        KeyStore.create(Paths.get("/tmp/test"), '', '')
+        then:
+        def ex = thrown(IllegalArgumentException)
+        ex.message == 'Unable to determine key type by file: /tmp/test'
+    }
+
+    def "Create with type"() {
+        expect:
+        KeyStore.create(Paths.get("/tmp/test.jks"), '', '', 'PKCS12').type == 'PKCS12'
+    }
+}
