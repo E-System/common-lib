0962b9f9eb89e38343562bea5ef9c5fb612d8793
diman <zuzoev.d@ext-system.com>
2020-02-22 10:41:26 +0700

    Add FileName class for split filename and extension

---
+++ src/main/java/com/es/lib/common/file/FileName.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016 E-System LLC
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.es.lib.common.file;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.apache.commons.io.FilenameUtils;
+
+import java.nio.file.Path;
+
+@Getter
+@ToString
+@RequiredArgsConstructor
+public class FileName {
+
+    private final String name;
+    private final String ext;
+
+    public static FileName create(String fileName) {
+        return new FileName(
+            FilenameUtils.getBaseName(fileName),
+            FilenameUtils.getExtension(fileName).toLowerCase()
+        );
+    }
+
+    public static FileName create(Path file) {
+        return create(file.toString());
+    }
+}
---
+++ src/test/groovy/com/es/lib/common/file/FileNameSpec.groovy
@@ -0,0 +1,48 @@
+package com.es.lib.common.file
+
+import spock.lang.Specification
+
+import java.nio.file.Paths
+
+class FileNameSpec extends Specification {
+
+    def "Only name"() {
+        when:
+        def res = FileName.create('fileName')
+        then:
+        res.name == 'fileName'
+        res.ext == ''
+    }
+
+    def "Only name with path"() {
+        when:
+        def res = FileName.create('/tmp/fileName')
+        then:
+        res.name == 'fileName'
+        res.ext == ''
+    }
+
+    def "Name and extension"() {
+        when:
+        def res = FileName.create('fileName.txt')
+        then:
+        res.name == 'fileName'
+        res.ext == 'txt'
+    }
+
+    def "Name and extension with path"() {
+        when:
+        def res = FileName.create('/tmp/fileName.txt')
+        then:
+        res.name == 'fileName'
+        res.ext == 'txt'
+    }
+
+    def "Name and extension from path"() {
+        when:
+        def res = FileName.create(Paths.get('/tmp/fileName.txt'))
+        then:
+        res.name == 'fileName'
+        res.ext == 'txt'
+    }
+}
