7a81b3d7a9543fec17caf66230be89fa785b0412
diman <zuzoev.d@ext-system.com>
2020-03-25 17:54:14 +0700

    Add FileResource

---
+++ src/test/groovy/com/es/lib/common/file/FileResourceSpec.groovy
@@ -0,0 +1,26 @@
+package com.es.lib.common.file
+
+import spock.lang.Specification
+
+class FileResourceSpec extends Specification {
+
+    def "GetFullPath (windows, internal)"() {
+        expect:
+        FileResource.create(true, false, "/path").fullPath == 'jar:/path'
+    }
+
+    def "GetFullPath (windows, external)"() {
+        expect:
+        FileResource.create(true, true, "/path").fullPath == 'file:////path'
+    }
+
+    def "GetFullPath (not windows, internal)"() {
+        expect:
+        FileResource.create(false, false, "/path").fullPath == 'jar:/path'
+    }
+
+    def "GetFullPath (not windows, external)"() {
+        expect:
+        FileResource.create(false, true, "/path").fullPath == 'file:/path'
+    }
+}
