299e0809c885677a4529a7401500568d245fbe99
diman <zuzoev.d@ext-system.com>
2022-12-08 16:30:53 +0700

    Add linked data convert options

--- src/main/java/com/es/lib/common/converter/option/LinkedCollectionConvertOption.java
+++ src/main/java/com/es/lib/common/converter/option/LinkedCollectionConvertOption.java
@@ -5,9 +5,14 @@ import lombok.NoArgsConstructor;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
 
 @NoArgsConstructor
 public class LinkedCollectionConvertOption<T, R> extends LinkedConvertOption<T, Collection<R>> {
+
+    public LinkedCollectionConvertOption(Map<T, Collection<R>> items) {
+        super(items);
+    }
 
     public LinkedCollectionConvertOption(T id, Collection<R> data) {
         super(id, data);
