42e14a7f670ea8beece94da5ee84d7b38e91d293
diman <zuzoev.d@ext-system.com>
2022-12-08 16:17:10 +0700

    Add linked data convert options

--- src/main/java/com/es/lib/common/converter/option/LinkedCollectionConvertOption.java
+++ src/main/java/com/es/lib/common/converter/option/LinkedCollectionConvertOption.java
@@ -3,16 +3,16 @@ package com.es.lib.common.converter.option;
 import com.es.lib.common.collection.Items;
 
 import java.util.Collection;
-import java.util.Map;
+import java.util.Collections;
 
 public class LinkedCollectionConvertOption<T, R> extends LinkedConvertOption<T, Collection<R>> {
 
-    public LinkedCollectionConvertOption(Map<T, Collection<R>> items) {
-        super(items);
-    }
-
     public LinkedCollectionConvertOption(T id, Collection<R> data) {
         super(id, data);
+    }
+
+    public LinkedCollectionConvertOption(T id, R data) {
+        super(id, Collections.singletonList(data));
     }
 
     public R getFirst(T id) {
