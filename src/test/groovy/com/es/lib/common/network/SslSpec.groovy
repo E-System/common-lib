1633a2644dc3211faabf888c5a3b737df6321d91
diman <zuzoev.d@ext-system.com>
2023-08-07 15:05:50 +0700

    test with p12

--- src/test/groovy/com/es/lib/common/network/SslSpec.groovy
+++ src/test/groovy/com/es/lib/common/network/SslSpec.groovy
@@ -1,8 +1,11 @@
 package com.es.lib.common.network
 
+import org.apache.commons.io.IOUtils
 import spock.lang.Specification
 
 import javax.net.ssl.X509TrustManager
+import java.nio.file.Files
+import java.nio.file.Path
 import java.security.NoSuchAlgorithmException
 
 class SslSpec extends Specification {
@@ -37,5 +40,17 @@ class SslSpec extends Specification {
         def verifier = Ssl.allowAllHostVerifier()
         then:
         verifier.verify("any", null)
+    }
+
+    def "p12"(){
+        when:
+        def file = Files.createTempFile("asd", ".p12")
+        Files.write(file, IOUtils.toByteArray(Ssl.getResourceAsStream("/com/es/cert/pkcs.p12")))
+        println file
+        def  keyStore = KeyStore.create(file, "1234567890", "1234567890")
+        def context = Ssl.context(keyStore)
+        then:
+        context != null
+        context.socketFactory != null
     }
 }
