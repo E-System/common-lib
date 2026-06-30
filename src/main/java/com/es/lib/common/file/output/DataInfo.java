773a01542c335ba4f94f12aeb84213a6a1829b9f
diman <zuzoev.d@ext-system.com>
2026-06-30 08:37:27 +0700

    Add DataInfo to OutputData

---
+++ src/main/java/com/es/lib/common/file/output/DataInfo.java
@@ -0,0 +1,14 @@
+package com.es.lib.common.file.output;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@RequiredArgsConstructor
+public class DataInfo {
+
+    private final String fileName;
+    private final String mime;
+}
--- src/main/java/com/es/lib/common/file/output/OutputData.java
+++ src/main/java/com/es/lib/common/file/output/OutputData.java
@@ -15,12 +15,20 @@
  */
 package com.es.lib.common.file.output;
 
+import com.es.lib.common.MimeUtil;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 
 /**
  * @author Dmitriy Zuzoev - zuzoev.d@ext-system.com
@@ -32,18 +40,6 @@ import java.nio.file.Path;
 public abstract class OutputData {
 
     private final String fileName;
-
-    public boolean isFile() {
-        return false;
-    }
-
-    public boolean isStream() {
-        return false;
-    }
-
-    public boolean isBytes() {
-        return false;
-    }
 
     /**
      * Create output data from file path
@@ -79,5 +75,51 @@ public abstract class OutputData {
      */
     public static OutputData create(String fileName, String contentType, byte[] content) {
         return new ByteData(fileName, contentType, content);
+    }
+
+    public boolean isFile() {
+        return false;
+    }
+
+    public boolean isStream() {
+        return false;
+    }
+
+    public boolean isBytes() {
+        return false;
+    }
+
+    public DataInfo info(){
+        if (isBytes()) {
+            ByteData data = (ByteData) this;
+            return new DataInfo(data.getFileName(), data.getContentType());
+        } else if (isStream()) {
+            StreamData data = (StreamData) this;
+            return new DataInfo(data.getFileName(), data.getContentType());
+        } else if (isFile()) {
+            FileData data = (FileData) this;
+            return new DataInfo(data.getFileName(), MimeUtil.get(data.getFileName()));
+        }
+        throw new IllegalArgumentException("Unsupported output data type");
+    }
+
+    public byte[] asBytes() throws IOException {
+        if (isBytes()) {
+            ByteData data = (ByteData) this;
+            return data.getContent();
+        } else if (isStream()) {
+            StreamData data = (StreamData) this;
+            return IOUtils.toByteArray(data.getContent());
+        } else if (isFile()) {
+            FileData data = (FileData) this;
+            Path path = StringUtils.isNotBlank(data.getRelativePath()) ? Paths.get(data.getRelativePath()) : null;
+            if (path != null) {
+                path = path.resolve(data.getContent());
+            }else{
+                path = data.getContent();
+            }
+            return Files.readAllBytes(path);
+        }
+        throw new IllegalArgumentException("Unsupported output data type");
     }
 }
