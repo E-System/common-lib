a0f808360919b40f25b2d3d0e8aa87aec29ffd55
diman <zuzoev.d@ext-system.com>
2026-07-25 13:32:15 +0700

    merge client info from develop

--- src/test/groovy/com/eslibs/common/model/ClientInfoSpec.groovy
+++ src/test/groovy/com/eslibs/common/model/ClientInfoSpec.groovy
@@ -98,4 +98,48 @@ class ClientInfoSpec extends Specification {
         result.appTimezone() == ZoneId.systemDefault()
         result.appLocale() == Locale.default
     }
+
+
+    def "Create with new headers all"() {
+        when:
+        def platform = 'iOS'
+        def platformVersion = 'iOS 13.0'
+        def appVersion = '1.0.0'
+        def guid = UUID.randomUUID().toString()
+        def headers = [
+                'es-app-platform'        : platform,
+                'es-app-platform-version': platformVersion,
+                'es-app-version'         : appVersion,
+                'es-app-timezone'        : 'GMT+03',
+                'es-app-locale'          : 'ru_RU',
+                'es-app-key'             : guid
+        ]
+        def result = ClientInfo.from(headers)
+        then:
+        result.platform() == ClientInfo.Platform.ios
+        result.platformVersion() == platformVersion
+        result.appVersion() == appVersion
+        result.appLocale() == Locale.default
+        result.appTimezone() == ZoneId.of('GMT+03')
+        result.appKey() == guid
+    }
+
+    def "Create with invalid platform"() {
+        when:
+        def platform = 'qwe'
+        def platformVersion = 'iOS 13.0'
+        def appVersion = '1.0.0'
+        def headers = [
+                'es-app-platform'        : platform,
+                'es-app-platform-version': platformVersion,
+                'es-app-version'         : appVersion
+        ]
+        def result = ClientInfo.from(headers)
+        then:
+        result.platform() == ClientInfo.Platform.undefined
+        result.platformVersion() == platformVersion
+        result.appVersion() == appVersion
+        result.appTimezone() == ZoneId.systemDefault()
+        result.appLocale() == Locale.default
+    }
 }
